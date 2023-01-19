6ce46d19b87ce9d6a7aa1b9dbbe6703aae806612
Sahooster <shuhaibntr@gmail.com>
2023-01-20 01:00:32 +0530

    mitsogo-theme_v0.8

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ $(document).ready(function(){
 
 
 
+// owl
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
